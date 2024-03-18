79553355ba9cc64dc8437fe119825f1575d0e38a
mellymell-appdev4ever <157563824+mellymell-appdev4ever@users.noreply.github.com>
2024-03-19 07:05:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
-import streamlit
-streamlit.title('My Parents New Healthy Diner')
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title("My Parents New Healthy Diner")
+st.write(
+    """Breakfast menu
+    """
+)
+st.write("Blueberry Oatmeal")
